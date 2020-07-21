8f64d81918869fe59e97324cf4b395ddd90274cf
Vu Dao <anhvudao17@gmail.com>
2020-07-21 22:08:49 +0300

    fix font for preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,7 @@
 import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
+import path from 'path'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
@@ -8,6 +9,8 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
+
+CMS.registerPreviewStyle(path.join(__dirname, '.cache', 'webfonts', 'webfonts.css'))
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
